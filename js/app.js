f7c5c904dd1fe85a0d3c734a1f84a891eacb60a7
hirata erika <biiiiii.uw@gmail.com>
2020-11-28 11:21:04 +0900

    あとはレスポンシブ整える

--- js/app.js
+++ js/app.js
@@ -1,18 +1,40 @@
-var mySwiper = new Swiper('.swiper-container', {
-    autoHeight: true,
-    // autoplay: {
-	// 	delay: 3000,
-	// 	stopOnLastSlide: false,
-	// 	disableOnInteraction: false,
-	// 	reverseDirection: false
-	// },
+// swiper
+
+// var mySwiper = new Swiper('.swiper-container', {
+//     autoHeight: true,
+//     // autoplay: {
+// 	// 	delay: 3000,
+// 	// 	stopOnLastSlide: false,
+// 	// 	disableOnInteraction: false,
+// 	// 	reverseDirection: false
+// 	// },
+// 	navigation: {
+// 		nextEl: '.swiper-button-next',
+// 		prevEl: '.swiper-button-prev'
+// 	},
+// 	pagination: {
+// 		el: '.swiper-pagination',
+// 		type: 'bullets',
+// 		clickable: true
+// 	}
+// });
+
+var swiper = new Swiper('.swiper-container', {
 	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev'
+	  nextEl: '.swiper-button-next',
+	  prevEl: '.swiper-button-prev',
 	},
+	slidesPerView: 4,
+	spaceBetween: 0,
+	initialSlide: 2,
+	loop: true,
 	pagination: {
 		el: '.swiper-pagination',
 		type: 'bullets',
-		clickable: true
-	}
-});
+		clickable: true,
+	  },
+	autoplay: {
+		delay: 3000,
+		disableOnInteraction: true
+	},
+  });
